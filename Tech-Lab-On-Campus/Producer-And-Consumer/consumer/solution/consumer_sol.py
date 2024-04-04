31e4d3bfbdb1abb4cfc5a180ae4508fafd924f50
cfandoh123 <138801418+cfandoh123@users.noreply.github.com>
2024-04-04 19:41:09 -0400

    Working Consumer

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,72 @@
+import pika
+from consumer_interface import mqConsumerInterface
+import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+class mqConsumer(mqConsumerInterface):
+    def __init__(
+        self, binding_key: str, exchange_name: str, queue_name: str
+    ) -> None:
+        self.routing_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        self.m_pool = ThreadPoolExecutor(max_workers=1)
+        self.setupRMQConnection()
+        pass
+
+    def setupRMQConnection(self) :
+        # Set-up Connection to RabbitMQ service
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        self.m_connection = pika.BlockingConnection(parameters=conParams)
+        self.m_channel = self.m_connection.channel()
+
+        # Establish Channel
+        self.m_exchange = 'RMQ Labs - Calvin/Alexander'
+        self.m_channel.exchange_declare(self.m_exchange)
+        
+        #Create the queue if not already present
+        self.m_queue_name = 'RMQ Lab Queue - Calvin/Alexander'
+        self.m_channel.queue_declare(queue=self.m_queue_name)
+        self.m_channel.queue_bind(queue=self.m_queue_name, routing_key=self.routing_key, exchange=self.m_exchange)
+        self.m_channel.basic_consume(self.m_queue_name, self.on_message_callback)
+
+        pass
+
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ):
+        
+        # Acknowledge message
+        channel.basic_ack(method_frame.delivery_tag)
+
+        #Print message (The message is contained in the body parameter variable)
+        print(f"Incoming Data. Method_Frame:{method_frame}\nHeader_Frame:{header_frame}\nBody:{body}")
+        if self.m_message_handler:
+            self.m_message_handler(body)
+
+        
+        pass
+
+    def consumeBlock(self):
+        try:
+            self.m_channel.start_consuming()
+        except KeyboardInterrupt:
+            self.m_channel.stop_consuming()
+
+    def startConsuming(self):
+        # Print " [*] Waiting for messages. To exit press CTRL+C"
+        print("[*] Waiting for messages. To exit press CTRL+C")
+        # Start consuming messages
+        self.m_pool.submit(self.consumeBlock)
+        pass
+
+    def __del__(self):
+        # Print "Closing RMQ connection on destruction"
+        print("Closing RMQ connection on destruction")
+        # Close Channel
+        self.m_channel.stop_consuming()
+        # Close Connection
+        self.m_pool.shutdown()
+        
+        pass
